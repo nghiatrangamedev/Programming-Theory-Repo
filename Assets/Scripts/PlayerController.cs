a52cd9fd4ffb2d55c49ddbbf56aed496b08496fd
nghiatran98 <nghiatran19980613@gmail.com>
2022-06-05 12:22:27 +0700

    initial commit
    empty 3d project on Unity 2020LTS

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float horizontalInput;
+    float speed = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 
+}
